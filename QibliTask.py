65ef124a97e0638c19d4d03cf268faac20a12f92
SmolUpsilon <83137147+SmolUpsilon@users.noreply.github.com>
2021-04-28 17:06:31 +0100

    Add files via upload
    This should be code for the assignment or third task.

---
+++ QibliTask.py
@@ -0,0 +1,12 @@
+list = ("keyboards", "bottles", "tea packets", "coffee beans", "commits") #Tried having the dictionary first with line 12 then line 3 after and it was messy, so I figured this would work better
+print(list)
+print("Insert the number of each item on the list, one at a time")
+
+dictionary = {
+            "keyboards": input(),
+            "bottles": input(),
+            "tea packets": input(),
+            "coffee beans": input(),
+            "commits": input()
+}
+print(dictionary)
